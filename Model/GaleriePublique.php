05b060911eabdf8c0efeb72eecc448c03477559f
FSVOBODA <fsvoboda@free.fr>
2015-02-12 09:57:57 +0100

    tte

---
+++ Model/GaleriePublique.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Franck
+ * Date: 04/02/2015
+ * Time: 00:11
+ */
+
+class GaleriePublique extends AppModel {
+
+    Var $useTable = "galeriespubliques";
+}
